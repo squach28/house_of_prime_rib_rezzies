156a999c5441d1cbcb848668b33222e63f8fc4d9
squach28 <squach3443@gmail.com>
2024-03-25 16:49:45 -0700

    add calendar with limitations (no past dates, only allow one year in advance)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
-import { MenuItem, Select, SelectChangeEvent } from "@mui/material"
+import { Box, MenuItem, Select, SelectChangeEvent } from "@mui/material"
 import Navbar from "./components/Navbar"
 import { useEffect, useState } from "react"
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers"
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import dayjs from "dayjs"
 
 
 const App = () => {
   const [authToken, setAuthToken] = useState<string>('')
   const [numOfPeople, setNumOfPeople] = useState<number>(2)
+  const [date, setDate] = useState<dayjs.Dayjs>(dayjs())
   const numOfPeopleRange = [2, 3, 4, 5, 6, 7, 8] // reservations are only allowed for parties of 2 up to 8
+  const MAX_ADVANCE_RESERVATION_TIME = 365 // reservations are only allowed one year in advance
 
   useEffect(() => {
 
@@ -48,15 +53,29 @@ const App = () => {
   return(
     <div className="min-h-screen p-4">
       <Navbar />
-      <Select
-        defaultValue={numOfPeople.toString()}
-        value={numOfPeople.toString()}
-        onChange={onSelectChange}
+      <Box 
+        sx={{
+          display: 'flex'
+        }}
       >
-        {numOfPeopleRange.map(num => (
-          <MenuItem key={num} value={num}>{`${num} people`}</MenuItem>
-        ))}
-      </Select>
+        <Select
+          defaultValue={numOfPeople.toString()}
+          value={numOfPeople.toString()}
+          onChange={onSelectChange}
+        >
+          {numOfPeopleRange.map(num => (
+            <MenuItem key={num} value={num}>{`${num} people`}</MenuItem>
+          ))}
+        </Select>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            defaultValue={date}
+            disablePast={true}
+            maxDate={dayjs().add(365, 'day')}
+          />
+        </LocalizationProvider>
+      </Box>
+
     </div>
   )
 }
