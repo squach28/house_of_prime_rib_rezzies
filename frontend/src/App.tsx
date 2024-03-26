46b13f3ad5d56c7bf68483ca9dd6c6994fd00936
squach28 <squach3443@gmail.com>
2024-03-26 13:58:19 -0700

    retrieve time slots, display time slots if available

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,38 @@
-import { Box, Button, MenuItem, Select, SelectChangeEvent } from "@mui/material"
-import Navbar from "./components/Navbar"
+import { Box, Button, CircularProgress, List, ListItem, MenuItem, Select, SelectChangeEvent } from "@mui/material"
 import { useEffect, useState } from "react"
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers"
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import dayjs from "dayjs"
+import Sad from './assets/images/sad.png'
+import Happy from './assets/images/happy.webp'
 
 
 const App = () => {
-  const [authToken, setAuthToken] = useState<string>('')
   const [numOfPeople, setNumOfPeople] = useState<number>(2)
   const [date, setDate] = useState<dayjs.Dayjs>(dayjs())
-  const [result, setResult] = useState()
+  const [loading ,setLoading] = useState<boolean>(false)
+  const [result, setResult] = useState<string[]>([])
   const numOfPeopleRange = [2, 3, 4, 5, 6, 7, 8] // reservations are only allowed for parties of 2 up to 8
   const MAX_ADVANCE_RESERVATION_TIME = 365 // reservations are only allowed one year in advance
 
   useEffect(() => {
-    const getAuthToken = (html: string) => {
-      const re = /"(.*?)"/g
-      const matches = [...html.matchAll(re)]
-      for(let i = 0; i < matches.length; i++) {
-        if(matches[i][0] === '"authToken"') {
-          return matches[i+1][0].split('"')[1]
-        }
-      }
-      return ''
-    }
-
-    const fetchAuthToken = () => {
-      fetch('https://www.opentable.com/restref/client/?restref=1779')
-        .then(res => res.text())
-        .then(html => {
-          const authToken = getAuthToken(html)
-          localStorage.setItem('authToken', authToken)
-          return authToken
-        })
-    }
-
-    if(localStorage.getItem('authToken')) {
-      const authToken = localStorage.getItem('authToken') as string
-      setAuthToken(authToken)
-    } else {
-      fetchAuthToken()
-    }
+    const today = dayjs()
+    const initialPartySize = 2
+    setLoading(true)
+    fetch(`${import.meta.env.VITE_API_URL}?date=${formatDate(today)}&partySize=${initialPartySize}`)
+      .then(res => res.json())
+      .then(data => { 
+        setResult(data)
+        setLoading(false)
+      })
   }, [])
+
+  const fetchTimeSlots = async () => {
+    const timeSlotUrl = `${import.meta.env.VITE_API_URL}?date=${formatDate(date)}&partySize=${numOfPeople}`
+    const res = await fetch(timeSlotUrl)
+    const timeSlots = await res.json()
+    return timeSlots
+  }
 
   const onSelectChange = (e: SelectChangeEvent) => {
     setNumOfPeople(parseInt(e.target.value))
@@ -53,43 +43,34 @@ const App = () => {
       setDate(value)
     }
   }
-  
-  const fetchTimeSlots = async () => {
-    const timeSlotUrl = 'https://www.opentable.com/restref/api/availability?lang=en-US'
-    const timeSlotRequests = []
-    const HOURS_IN_DAY = 24
-    for(let i = 0; i < HOURS_IN_DAY; i++) {
-      const hour = i.toString().padStart(2, '0')
 
-      const body = JSON.stringify({
-        rid: 1779,
-        dateTime: `${date.format('YYYY-MM-DD')}T${hour}:00`,
-        partySize: numOfPeople
-      })
-      const promise = fetch(timeSlotUrl, {
-        method: 'POST',
-        headers: {
-          'Authorization': `Bearer ${authToken}`,
-          'Content-Type': 'application/json'
-        },
-        body
-      })
-        .then(res => res.json())
-      timeSlotRequests.push(promise)
-    }
-    const promises = await Promise.all(timeSlotRequests)
-    console.log(promises)
+  const formatDate = (date: dayjs.Dayjs) => {
+    const formatted = date.toDate().toISOString().split('T')[0]
+    return formatted
   }
 
-  fetchTimeSlots()
-
+  const onSearchClick = async () => {
+    try {
+      setLoading(true)
+      const res = await fetchTimeSlots()
+      setResult(res)
+    } catch(e) {
+      console.log(e)
+    } finally {
+      setLoading(false)
+    }
+  }
+  
 
   return(
-    <div className="min-h-screen p-4">
-      <Navbar />
+    <div className="w-full flex flex-col min-h-screen p-4 bg-[#FAF9F6]">
+      <h1 className="text-3xl text-center my-8 font-bold">
+        House of Prime Rib Rezzies
+      </h1>
       <Box 
         sx={{
-          display: 'flex'
+          display: 'flex',
+          marginX: 'auto'
         }}
       >
         <Select
@@ -112,15 +93,78 @@ const App = () => {
         </LocalizationProvider>
         <Button
           variant="outlined"
+          onClick={onSearchClick}
         >
           Search
         </Button>
       </Box>
-      {result 
-          ?
-            <div>result</div>
-          :
-            <div>no result</div>
+      {loading ? 
+        <Box 
+          sx={{
+            marginTop: 2,
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center'
+          }}
+        >
+          <CircularProgress />
+        </Box>
+        :
+            <Box 
+            sx={{
+              marginTop: 2,
+              width: '100%',
+              display: 'flex',
+              justifyItems: 'center',
+              alignItems: 'center',
+              flexDirection: 'column',
+              justifyContent: 'center'
+            }}
+          >
+            {
+              result.length > 0 
+              ?
+                <>
+                  <h2 className="text-xl font-bold my-5"
+                  >
+                    Yippee! There's time slots available :3
+                  </h2>
+                  <img className="max-w-[50%] my-4" src={Happy} alt="smiling man with ok hand" />
+                  <List sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '100%',
+                    flex: 1
+                  }}>
+                    {result.map(time => (
+                      <ListItem key={time} sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        width: '100%',
+                        flex: 1
+                        
+                      }}>
+                        <p className="p-4 text-md bg-[#ABF0D1] rounded-md text-center font-bold min-w-[30%]">
+                          {new Date(time).toLocaleDateString('en-US', {
+                            hour: 'numeric',
+                            minute: 'numeric'
+                          })}
+                        </p>
+                      </ListItem>
+                    ))}
+                  </List>
+                </>
+              :
+                <>
+                  <h2 className="text-2xl font-bold my-5">
+                    No available time slots for this date 
+                  </h2>
+                  <img src={Sad} alt="crying emoji" />
+                </>
+            }
+          </Box>
         }
 
     </div>
--- frontend/src/components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-const Navbar = () => {
-  return (
-    <ul className="flex justify-between">
-        <li>House of Prime Rib Rezzies</li>
-    </ul>
-  )
-}
-
-export default Navbar
